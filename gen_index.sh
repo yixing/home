632edd48b1a162e3f9e48a91d6fd08385e4db212
yixing <yixing@yixingdeMacBook-Pro.local>
2019-12-03 17:58:14 +0800

    update index

--- gen_index.sh
+++ gen_index.sh
@@ -1,3 +1,3 @@
 echo '# index' > README.md
 
-for l in `find blog|grep -v "blog$"|sed -n -e 's/[^\/]*\/\(.*\)\.md/\1/p'`;do echo "* [$l](http://yixing.github.io/blog/$l.html)" ;done >> README.md 
+for l in `find blog|grep -v "blog$"|sed -n -e 's/[^\/]*\/\(.*\)\.md/\1/p'|sed -e 's/README//'`;do echo "* [$l](http://yixing.github.io/blog/$l.html)" ;done >> README.md 
